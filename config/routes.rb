a05389ce93b167493f737182f61eedd0ffa6044b
Mira15 <mirjamkoerner@gmail.com>
2016-12-19 19:59:03 +0100

    5.1 reroute root page, redirect user, create dynamic links, update links in nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   resources :orders, only: [:index, :show, :create, :destroy]
   resources :products
-  get 'static_pages/about'
+  get '/about', to: 'static_pages#about'
 
-  get 'static_pages/contact'
+  get '/contact', to: 'static_pages#contact'
 
-  root 'static_pages#landing_page'
+  get '/caroussel', to: 'static_pages#landing_page'
+
+  root 'static_pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
