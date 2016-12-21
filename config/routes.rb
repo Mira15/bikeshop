492f974b995ba858b9f0d71cf8363fdccd3191d2
Mira15 <mirjamkoerner@gmail.com>
2016-12-21 20:21:13 +0100

    create signup and login pages for user with gem Devise, little layout for views of authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+
+  resources :users
+
   post 'static_pages/thank_you'
 
   resources :orders, only: [:index, :show, :create, :destroy]
