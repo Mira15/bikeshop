0726bba45f779e1ec807d3c6134044dcb60f3b82
Mira15 <mirjamkoerner@gmail.com>
2017-01-05 14:15:16 +0100

    6.2 bug fixed RSpec file for Comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,11 +4,11 @@ describe Comment do
   before do
     @product = Product.create!(name: "race bike")
     @user = User.create!(email: "me@example.com", password: "passwort")
-    @comment = @product.comments.create!(rating: 5, user: @user, body: "This bike is perfect")
+    @comment = @product.comments.new(rating: 5, user: @user, body: "This bike is perfect")
   end
 
   it "is valid" do
-    expect(Comment.new(@comment)).to be_valid
+    expect(@comment).to be_valid
   end
 
 end
